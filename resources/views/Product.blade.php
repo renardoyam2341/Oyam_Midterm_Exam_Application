1d367439b475d4977701d66c233aee3ad04205be
Rubs <hyperfury409@gmail.com>
2024-02-21 12:27:40 +0800

    Update: Created ProductController.php and Product.blade.php

---
+++ resources/views/Product.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+<style>
+    table{
+        background-color: skyblue;
+        width:500px;
+        height:500px;
+    }
+    td,th{
+        font-size:50px;
+        border:solid;
+    }
+</style>
+<body>
+    <h1>Movies</h1>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Ranking</th>
+      <th scope="col">Views</th>
+      <th scope="col">Rating</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Morbius</th>
+      <td>1</td>
+      <td>99,999,999,999</td>
+      <td>10.0</td>
+    </tr>
+    <tr>
+      <th scope="row">Bee movie</th>
+      <td>10</td>
+      <td>13,258,159</td>
+      <td>7.9</td>
+    </tr>
+    <tr>
+      <th scope="row">Avatar</th>
+      <td>119</td>
+      <td>10,999</td>
+      <td>6.2</td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
